e247534f46ebd3f8b9a6f5a4db3dc9ee718d07b6
hsxazj <2279538834@qq.com>
2026-01-09 09:37:02 +0800

    remove(jwxt): 移除教务系统相关代码文件
    - 删除 client.ts 中的 HTTP 客户端配置
    - 删除 credentials.ts 中的凭证管理功能
    - 删除 getCourse.ts 中的课程信息获取接口
    - 删除 getExam.ts 中的考试信息获取接口
    - 删除 getGrade.ts 中的成绩信息获取接口
    - 删除 getInfo.ts 中的学生信息获取接口
    - 删除 getStartDate.ts 中的学期开始日期获取功能
    - 删除 http.ts 中的 HTTP 请求工具类
    - 删除 login.ts 中的登录认证逻辑
    - 删除 logout.ts 中的登出功能实现

--- dev-doc/jwxt/api/logout.ts
+++
@@ -1,7 +0,0 @@
-import { http } from "@/lib/jwxt/client";
-
-export async function logout(): Promise<void> {
-  const url = "logout";
-  const referer = "xtgl/index_initMenu.html";
-  await http.post<any>(url, { referer });
-}
